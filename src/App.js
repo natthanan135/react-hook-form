90b2a20f4f014b0afd967e00cd204140271f6e96
natthanan135 <natthanan.sa.65@ubu.ac.th>
2023-08-15 14:54:54 +0700

    react-hook-form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
+import { Button, Container, Form, FormText } from "react-bootstrap";
+import { useForm } from "react-hook-form";
 
 function App() {
+  const { register, handleSubmit } = useForm();
+  const [data, setData] = useState();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <h4>React Hook From</h4>
+      <Form onSubmit={handleSubmit((data) => setData(JSON.stringify(data)))}>
+        <Form.Group className="mb-3">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            {...register("email")}
+            type="email"
+            placeholder="Enter email"
+          />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            {...register("password")}
+            type="password"
+            placeholder="Enter password"
+          />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label>ประเภทสมาชิก</Form.Label>
+          <Form.Select {...register("useType")}>
+            <option value="1" selected>
+              ทั่วไป
+            </option>
+            <option value="2">VIP</option>
+            <option value="3">ผู้ดูเเลระบบ</option>
+          </Form.Select>
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Check
+            {...register("remember")}
+            type="checkbox"
+            label="Remember me"
+          />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <FormText>{data}</FormText>
+        </Form.Group>
+        <Button type="submit">Submit</Button>
+      </Form>
+    </Container>
   );
 }
 
